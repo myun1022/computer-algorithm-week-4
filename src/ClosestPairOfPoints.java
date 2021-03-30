9aaefc0874013256a3c74a8b91f711b6c88e9a2b
jyajoo <jyajoo1020@gmail.com>
2021-03-30 19:18:15 +0900

    divcon

--- src/ClosestPairOfPoints.java
+++ src/ClosestPairOfPoints.java
@@ -1,4 +1,4 @@
-//03.29(MON) : 랜덤 N개의 점 X좌표 오름차순 정렬 구현 (합병 정렬)
+//03.29(Mon) : 랜덤 N개의 점 X좌표 오름차순 정렬 구현 (합병 정렬)
 
 import java.util.Random;
 
@@ -37,6 +37,44 @@ public class ClosestPairOfPoints {
 
     }
 
+    static double distance(int[][] A, int i, int j) { // 두 점 사이의 거리 계산
+        return Math.sqrt(Math.pow(A[i][0] - A[j][0],2) + Math.pow(A[i][1] - A[j][1],2));
+    }
+
+    static double points(int [][]A){
+        int size = A.length;
+        if (size == 2) {                                  //점이 2개일 경우 그대로 점 출력
+            System.out.println(A[0][0] + " " + A[0][1]);
+            System.out.println(A[1][0] + " " + A[1][1]);
+        }
+        else if (size == 3) {                              //점이 3개일 경우 그 중에 가장 근접한 2개의 점 출력(동일한 거리가 존재하면 하나의 쌍만 출력됩니다.)
+            double close = distance(A, 0, 1), d;
+            int a = 0, b = 0;
+            for (int i = 0; i < size; i++) {
+                for (int j = i+1; j < size; j++) {
+                    d = distance(A, i, j);
+                    if (d < close) { close = d; a = i; b = j; }
+                }
+            }
+            System.out.println(A[a][0] + " " + A[a][1]);
+            System.out.println(A[b][0] + " " + A[b][1]);
+        }
+        return 0;
+    }
+
+    static double div_con(int[][] A, int left, int right) {  // 아직 중앙부분 추가 전이라 점 3개 초과일 경우 오류 발생합니다.
+        int size = A.length;
+        if (size <= 3)
+            return points(A);
+
+        int mid = (left + right) / 2;
+        double CP_left = div_con(A, left ,mid);
+        double CP_right = div_con(A, mid + 1, right);
+        double min_dist = Math.min(CP_left, CP_right);
+
+        return min_dist;
+    }
+
     public static void main(String[] args) {
 
         Random r = new Random();
@@ -49,5 +87,10 @@ public class ClosestPairOfPoints {
 
         ClosestPairOfPoints sorter = new ClosestPairOfPoints();
         sorter.mergeSort(arr, 0, arr.length - 1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+        System.out.println("--");
+        div_con(arr, 0, arr.length -1);
     }
 }
