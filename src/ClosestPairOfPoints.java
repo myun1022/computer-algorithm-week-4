a3d6eaf57089d44fa5d8dda01e1d2c8b9b93e367
champgj <champ3415@naver.com>
2021-03-30 12:40:22 +0900

    code move and hamsu chuga

--- src/ClosestPairOfPoints.java
+++ src/ClosestPairOfPoints.java
@@ -1,65 +1,88 @@
-import java.util.Arrays;
-import java.util.Scanner;
+
+
+//03.29(MON) : 랜덤 N개의 점 X좌표 오름차순 정렬 구현 (합병 정렬)
+
+import java.util.Random;
 
 public class ClosestPairOfPoints {
-    public static Point p[];
 
-    public static class Point {
-        int x;
-        int y;
-        Point( int x, int y ) {
-            this.x = x;
-            this.y = y;
+    public static void mergeSort(int[][] A, int p, int q) {
+        if (p >= q) return;       //배열의 원소의 수가 2개 이상이여야 함
+        int k = (p + q) / 2;      //중간
+
+        mergeSort(A, p, k);         //앞 부분
+        mergeSort(A, k + 1, q);  //뒷 부분
+        merge(A, p, k, q);          //합병
+    }
+
+    public static void merge(int[][] A, int p, int k, int q) {
+        int[][] C = new int[q - p + 1][2];
+        int i = p, j = k + 1, n = 0;
+
+        while (i <= k && j <= q) {
+            C[n][0] = A[i][0] < A[j][0] ? A[i][0] : A[j][0];          // x좌표가 더 작은 값 대입
+            C[n++][1] = A[i][0] < A[j][0] ? A[i++][1] : A[j++][1];    // y좌표 대입
         }
-    }
-    static int getDistancejegob( Point p1, Point p2 ) {
-        return (p1.x-p2.x) * (p1.x-p2.x) + (p1.y-p2.y) * (p1.y-p2.y);
-    }
+        while (i <= k) {
+            C[n][0] = A[i][0];
+            C[n++][1] = A[i++][1];
+        }
+        while (j <= q) {
+            C[n][0] = A[j][0];
+            C[n++][1] = A[j++][1];
+        }
 
-    static int divideConquer( int left, int right ) {
+        for (int a = p, b = 0; a <= q; a++) {    //기존 배열(A)에 반영
+            A[a][0] = C[b][0];
+            A[a][1] = C[b++][1];
+        }
 
-        int mid = ( left + right ) / 2;
-        int dleftsec = divideConquer( left, mid );		// left section distance
-        int drightsec = divideConquer( mid+1, right );	// right section distance
+    }
+/*
+    static int divideConquer(int left, int right) {
+
+        int mid = (left + right) / 2;
+        int dleftsec = divideConquer(left, mid);        // 왼쪽부터 중간
+        int drightsec = divideConquer(mid + 1, right);    // 중간부터 오른쪽
 
         int dMin = dleftsec < drightsec ? dleftsec : drightsec;
 
         return dMin;
     }
-
-    //팀플
-
+*/
+    static int getDistanceJegob(int[][] A, int a, int b) {
+        return (A[a][0] - A[b][0]) * (A[a][0] - A[b][0]) + (A[a][1] - A[b][1]) * (A[a][1] - A[b][1]);
+    }
 
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        p = new Point[n];
-        for( int i = 0; i < n; i++ ) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            p[i] = new Point(a,b);
+        Random r = new Random();
+        int[][] arr = new int[r.nextInt(10)][2];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][0] = r.nextInt(10); //x좌표
+            arr[i][1] = r.nextInt(10); //y좌표
+            System.out.println(arr[i][0] + " " + arr[i][1]); //정렬 전 출력
         }
 
 
-        Arrays.sort( p, ( p1,p2 ) -> 
-        {
-            return (p1.x - p2.x);
-        });
+        System.out.println("정렬해보자!");
+        ClosestPairOfPoints sorter = new ClosestPairOfPoints();
+        sorter.mergeSort(arr, 0, arr.length - 1);
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1] +" ");
+           // if(i!= arr.length){
+           //     System.out.println("점사이 거리 제곱은 "+getDistanceJegob(arr,i,i+1));//제곱거리 함수확인코드
+           // }
 
-        for(int i = 0 ; i<n; i++){
-            System.out.printf("%d %d\n",p[i].x,p[i].y);
         }
-
 
     }
-
-    //21.03.28 우선은 X축 정렬 까지만 작업
-
-
-
-
-
 }
+
+
+// 출력확인코드-> 아예 안나오네?..
+/*+ getDistanceJegob(arr,i,i+1 )
+ *
+ *
+ * */
---
+++ src/maintest.java
@@ -0,0 +1,65 @@
+//3.29(월) : 랜덤 n개의 점 배치 후, 좌, 우 분할을 위해 정렬해주기.
+//2차원 배열, 합병 정렬 이용
+// -> 완성 x..ㅠㅠ
+
+import java.util.Random;
+
+public class maintest {
+
+    public static void mergeSort(int[][] A, int p, int q) {
+        if (p >= q) return;  //배열의 원소의 수가 2개 이상이여야 함
+        int k = (p + q) / 2; //중간 값
+        mergeSort(A, p, k);  //앞부분
+        mergeSort(A, k + 1, q); //뒷부분
+        merge(A, p, k, q); //합병
+    }
+
+    public static void merge(int[][] A, int p, int k, int q) { //합병
+        int[][] C = new int[q - p + 1][2];
+
+        int i = p, j = k + 1, n = 0;
+        while (i <= k && j <= q) {
+            C[n][0] = A[i][0] < A[j][0] ? A[i][0] : A[j][0]; //작은 값 대입
+            C[n][1] = A[i][0] < A[j][0] ? A[i++][1] : A[j++][1];
+            n++;
+        }
+        while (i <= k) {
+            C[n][0] = A[i][0];
+            C[n][1] = A[i][1];
+            n++;
+            i++;
+        }
+        while (j <= q) {
+            C[n][0] = A[j][0]; //남은 값 대입
+            C[n][1] = A[j][1];
+            n++;
+            j++;
+        }
+
+        for (int a = p, b = 0; a <= q; a++) {
+            A[a][0] = C[b][0]; //기존 배열 위치 변경
+            A[a][1] = C[b][1];
+            b++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[][] arr = new int[r.nextInt(10)][2];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][0] = r.nextInt(10); //x좌표
+            arr[i][1] = r.nextInt(10); //y좌표
+            System.out.println(arr[i][0] + " " + arr[i][1]); //정렬 전 출력
+        }
+
+
+        System.out.println("정렬해보자!");
+        maintest sorter = new maintest();
+        sorter.mergeSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
